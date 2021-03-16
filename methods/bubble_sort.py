56173ab5c2d9284d465b568e1df582a0119431d1
Artur <artur.strz@wp.pl>
2021-03-16 20:30:19 +0100

    delete matplotlib visualization

--- methods/bubble_sort.py
+++ methods/bubble_sort.py
@@ -1,8 +1,3 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import random as r
-
-
 def bubble_sort(numbers):
     length_array = len(numbers)
     swapped = True
@@ -16,35 +11,4 @@ def bubble_sort(numbers):
                 numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
                 swapped = True
             yield numbers
-
-
-def bubble_sort_visualize(size, speed):
-    numbers = [x + 1 for x in range(size)]
-    r.shuffle(numbers)
-
-    fig, ax = plt.subplots()
-    bar_list = ax.bar(range(len(numbers)), numbers, width=0.8)
-    iteration = [0]
-
-    def update(array, bars, i):
-        for bar, value in zip(bars, array):
-            index = array.index(value)
-            # set colors
-            try:
-                if value > array[index + 1]:
-                    bar.set_color('red')
-                else:
-                    bar.set_color('green')
-            except IndexError:
-                bar.set_color('green')
-
-            # set value
-            bar.set_height(value)
-
-        i[0] += 1
-
-    anim = animation.FuncAnimation(fig, func=update, fargs=(bar_list, iteration), frames=bubble_sort(numbers),
-                                   interval=speed, repeat=True, save_count=1000)
-    plt.show()
-
 
--- methods/odd_even_sort.py
+++ methods/odd_even_sort.py
@@ -1,8 +1,3 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import random as r
-
-
 def odd_even_sort(numbers):
     sort = False
     while sort is False:
@@ -18,31 +13,3 @@ def odd_even_sort(numbers):
                 numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
                 sort = False
                 yield numbers
-
-
-def odd_even_sort_visualize(size, speed):
-    # speed {15, 50, 80}
-    numbers = [x + 1 for x in range(size)]
-    print(numbers)
-    r.shuffle(numbers)
-
-    fig, ax = plt.subplots()
-    bar_list = ax.bar(range(len(numbers)), numbers, width=0.8)
-    iteration = [0]
-
-    def update(array, bars, i):
-        for bar, value in zip(bars, array):
-            index = array.index(value)
-
-            # set colors
-            if index % 2 == 0:
-                bar.set_color('green')
-            else:
-                bar.set_color('red')
-
-            bar.set_height(value)
-        i[0] += 1
-
-    anim = animation.FuncAnimation(fig, func=update, fargs=(bar_list, iteration), frames=odd_even_sort(numbers),
-                                   interval=speed, repeat=True, save_count=1000)
-    plt.show()
