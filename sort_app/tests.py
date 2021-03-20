ebcc42696a81e5ea22ed63032e9406d024312ea4
Artur <artur.strz@wp.pl>
2021-03-20 15:09:41 +0100

    create test to generate array view

--- sort_app/tests.py
+++ sort_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
+import random as r
 
-# Create your tests here.
+class Test(TestCase):
+    def test_generate_array(self):
+        number = r.randrange(10, 45)
+        response = self.client.post(
+            reverse('generate_array'),
+            data={'size': str(number)},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+        )
+        self.assertEqual(200, response.status_code)
+        array_str = response.content.decode('ascii').replace('[', '').replace(']', '').replace(',', '')
+        array = list(array_str.split(" "))         
+        self.assertEqual(len(array), number)
